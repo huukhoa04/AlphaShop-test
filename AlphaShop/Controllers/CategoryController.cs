548ddc6225def377aad633cbc8331a676fc1e4f6
huukhoa04 <119769327+huukhoa04@users.noreply.github.com>
2024-04-30 20:52:24 +0700

    New

---
+++ AlphaShop/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DrinkInfo()
+        {
+            return View();
+        }
+    }
+}
---
+++ AlphaShop/Controllers/LogController.cs
@@ -0,0 +1,86 @@
+﻿using AlphaShop.Data;
+using AlphaShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlphaShop.Controllers
+{
+    public class LogController : Controller
+    {
+        private readonly HahaContext _context;
+
+
+        public LogController(HahaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel loginModel)
+        {
+            var taikhoanForm = loginModel.Name;
+            var matkhauForm = loginModel.Password;
+            var usercheck = _context.Customers.SingleOrDefault(x => x.CtrLogusername == taikhoanForm && x.CtrPassword == matkhauForm);
+            if (usercheck != null)
+            {
+                TempData["Account"] = usercheck;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.LoginFail = "Failed to log";
+                return View();
+            }
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterModel registerModel)
+        {
+            var usercheck = _context.Customers.SingleOrDefault(x => x.CtrUsername == registerModel.Username);
+            if (usercheck != null)
+            {
+                ViewBag.RegisterFailed = "Failed to register, username has existed!";
+                return View();
+            }
+            else if (registerModel.Password != registerModel.ConfirmPassword)
+            {
+                ViewBag.RegisterFailed = "Password is not synced";
+                return View();
+            }
+
+            _context.Database.ExecuteSqlRawAsync($"set IDENTITY_INSERT dbo.CUSTOMER on;");
+            _context.Customers.AddAsync(
+                new Customer
+                {
+                    CtrLogusername = registerModel.LogUsername,
+                    CtrUsername = registerModel.Username,
+                    CtrPassword = registerModel.Password,
+                    CtrEmail = registerModel.Email,
+                    CtrPhonenumber = registerModel.PhoneNumber,
+                    CtrGender = registerModel.Gender,
+                    CtrAccess = 1,
+                    CtrId = _context.Customers.Count(),
+                    CtrUsed = 0,
+                    CtrImage = null
+                    
+
+                }
+            );
+            _context.SaveChanges();
+            _context.Database.ExecuteSqlRawAsync($"set IDENTITY_INSERT dbo.CUSTOMER off;");
+            _context.Database.BeginTransaction();
+            return RedirectToAction("Login");
+        }
+        public IActionResult Logout()
+        {
+            TempData["AccountName"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
